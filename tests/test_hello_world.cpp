f24d052818a55168d33df90c58f259664d9e304b
Leo Soares <leojrfs@gmail.com>
2025-02-15 16:51:12 +0000

    tests/test_hello_world: add fail test

--- tests/test_hello_world.cpp
+++ tests/test_hello_world.cpp
@@ -2,9 +2,20 @@
 #include <cassert>
 
 #include "hello_world.h"
- 
-int main()
-{
-    assert(getHelloWorld() == "hello world");
-    std::cout << "getHelloWorld() works\n";
+
+/* This is for creating a failing test for testing the test infrastructure */
+#ifndef FAIL_COMPARISON_STR
+#define FAIL_COMPARISON_STR ""
+#endif
+
+#define PASS_COMPARSION_STR "hello world"
+
+int main() {
+    auto ret_string = getHelloWorld();
+    if(0 == ret_string.compare(PASS_COMPARSION_STR FAIL_COMPARISON_STR)) {
+        std::cout << "PASS: " << ret_string << " = " << PASS_COMPARSION_STR << std::endl;
+    } else {
+        std::cout << "FAIL: " << ret_string << " != " << PASS_COMPARSION_STR << std::endl;
+        return 1;
+    }
 }
