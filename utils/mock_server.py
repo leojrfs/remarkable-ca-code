5cd241ee4026e4307b689bcbd6333fde32ccaa2a
Leo Soares <leojrfs@gmail.com>
2025-02-17 12:22:22 +0000

    utils/mock_server: pretty-print json request

--- utils/mock_server.py
+++ utils/mock_server.py
@@ -1,8 +1,9 @@
+import json
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/report', methods=['POST'])
+@app.route("/report", methods=["POST"])
 def report():
     try:
         print("request.user_agent=" + str(request.user_agent))
@@ -11,24 +12,28 @@ def report():
         data = request.get_json()
         if data is None:
             return jsonify({"error": "Invalid JSON"}), 400
-        
+
         # Validation: Ensure required keys exist
         required_fields = ["hostname", "uptime", "memory", "disk"]
         missing_fields = [field for field in required_fields if field not in data]
-        
+
         if missing_fields:
-            return jsonify({"error": f"Missing fields: {', '.join(missing_fields)}"}), 400
-        
+            return (
+                jsonify({"error": f"Missing fields: {', '.join(missing_fields)}"}),
+                400,
+            )
+
         # validate field types
         if not isinstance(data["hostname"], str) or not isinstance(data["uptime"], int):
             return jsonify({"error": "Invalid field types"}), 400
-        
-        print("jsonifyed_data=" + str(data))
+
+        print("jsonifyed_data=\n" + json.dumps(data, indent=4, sort_keys=True))
 
         return jsonify({"message": "Report received"}), 201
 
     except Exception as e:
         return jsonify({"error": str(e)}), 400
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8090, debug=True)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8090, debug=True)
